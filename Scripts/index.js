cddec9e83066216f8a692ececfbd0b5e93a30731
Belscki <136617733+Belscki@users.noreply.github.com>
2024-04-12 12:17:01 -0300

    Add files via upload

--- Scripts/index.js
+++ Scripts/index.js
@@ -2,15 +2,27 @@
 const Navegar = document.querySelector(".Navegar");
 const PaginaInicial = document.querySelector(".PaginaInicial");
 const Apresentacao = document.querySelector(".Apresentacao");
-
+const Conhecimento = document.querySelector(".Conhecimento");
+const NavegarMais = document.querySelector(".NavegarMais");
+const NavegarMaisMais = document.querySelector(".NavegarMaisMais");
+const Projetos = document.querySelector(".Projetos");
 
 Navegar.addEventListener('click', ()=>{
     PaginaInicial.classList.toggle('active');
     Navegar.classList.toggle('active');
 })
-
-
 Navegar.addEventListener('click', ()=>{
     Apresentacao.classList.toggle('active');
 })
+NavegarMais.addEventListener('click', ()=>{
+    Conhecimento.classList.toggle("active");
+    Apresentacao.classList.toggle("active");
+}) 
+NavegarMaisMais.addEventListener('click', ()=>{
+    Conhecimento.classList.toggle("active");
+    Projetos.classList.toggle("active");
+})
+
+
+
 
