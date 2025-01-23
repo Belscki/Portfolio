b00b190ddc4db75c5b4bc8105feb95c22c467b34
Belscki <136617733+Belscki@users.noreply.github.com>
2025-01-22 23:15:05 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -134,3 +134,15 @@ function animate() {
 animate()
 
 window.Sobremim = Sobremim
+
+var app = angular.module('App', []);
+
+app.controller('projetoController', function($scope){
+    $scope.projetos = [
+        { nome: "Projeto 1", descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, veniam perferendis, nulla fugit neque soluta saepe enim dolores placeat quisquam cum ut adipisci reprehenderit eius? Blanditiis qui non cupiditate vero!", projLink: "Link"},
+        { nome: "Projeto 2", descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, veniam perferendis, nulla fugit neque soluta saepe enim dolores placeat quisquam cum ut adipisci reprehenderit eius? Blanditiis qui non cupiditate vero!", projLink: "Link"},
+        { nome: "Projeto 3", descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, veniam perferendis, nulla fugit neque soluta saepe enim dolores placeat quisquam cum ut adipisci reprehenderit eius? Blanditiis qui non cupiditate vero!", projLink: "Link"},
+        { nome: "Projeto 4", descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, veniam perferendis, nulla fugit neque soluta saepe enim dolores placeat quisquam cum ut adipisci reprehenderit eius? Blanditiis qui non cupiditate vero!", projLink: "Link"},
+        { nome: "Projeto 5", descricao: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Hic, veniam perferendis, nulla fugit neque soluta saepe enim dolores placeat quisquam cum ut adipisci reprehenderit eius? Blanditiis qui non cupiditate vero!", projLink: "Link"}
+    ];
+})
